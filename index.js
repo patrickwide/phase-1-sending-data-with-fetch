06a7f1e1e33ce61b333c815f706673ae92a1a974
patrickwide <patrickwide254@gmail.com>
2023-03-31 04:41:26 +0300

    Add form and create a createForm function

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+  const url = "http://localhost:3000/users";
+  const data = { name, email };
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch(url, config)
+    .then((response) => response.json())
+    .then((json) => {
+      const id = json.id;
+      document.body.innerHTML += id;
+    })
+    .catch((error) => {
+      document.body.innerHTML += error.message;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const userForm = document.getElementById("user-form");
+  userForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // prevent the default form submission behavior
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    submitData(name, email);
+  });
+});
